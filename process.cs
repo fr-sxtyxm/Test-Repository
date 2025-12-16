b71b08dba214522a6daa75123a1c03c634b1a1c5
Satyam Maurya <randomIndianName@example.com>
2025-12-16 14:23:02 +0530

    Procedure of Git Commit

---
+++ process.cs
@@ -0,0 +1,6 @@
+// 1. Create a repo
+// 2. Clone locally in cmd
+// 3. Create a folder & type and enter cmd on it's address bar
+// 4. Open folder in VS Code
+// 5. Write a code and save the file.
+// 6. Using Source Control Panel, commit & push file into the repo adding a message
